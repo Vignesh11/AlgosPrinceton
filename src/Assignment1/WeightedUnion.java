e0f79a482c0c1ca5e0a5e6a2da80f76442b5a9fb
Vignesh <vickzzkct@gmail.com>
2015-07-01 07:28:18 +0530

    Adding Algorithm files

---
+++ src/Assignment1/WeightedUnion.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1;
+
+/**
+ *
+ * @author Vignesh
+ */
+public class WeightedUnion {
+  
+    private int[] parent;   // parent[i] = parent of i
+    private int[] size;     // size[i] = number of objects in subtree rooted at i
+    private int count;      // number of components
+
+    /**
+     * Initializes an empty union-find data structure with N isolated components 0 through N-1.
+     * @throws java.lang.IllegalArgumentException if N < 0
+     * @param N the number of objects
+     */
+    public WeightedUnion(int N) {
+        count = N;
+        parent = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    /**
+     * Returns the number of components.
+     * @return the number of components (between 1 and N)
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns the component identifier for the component containing site <tt>p</tt>.
+     * @param p the integer representing one site
+     * @return the component identifier for the component containing site <tt>p</tt>
+     * @throws java.lang.IndexOutOfBoundsException unless 0 <= p < N
+     */
+    public int find(int p) {
+        validate(p);
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int N = parent.length;
+        if (p < 0 || p >= N) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + N);
+        }
+    }
+
+    /**
+     * Are the two sites <tt>p</tt> and <tt>q</tt> in the same component?
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @return <tt>true</tt> if the two sites <tt>p</tt> and <tt>q</tt>
+     *    are in the same component, and <tt>false</tt> otherwise
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+  
+    /**
+     * Merges the component containing site<tt>p</tt> with the component
+     * containing site <tt>q</tt>.
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make smaller root point to larger one
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        count--;
+    }
+}
